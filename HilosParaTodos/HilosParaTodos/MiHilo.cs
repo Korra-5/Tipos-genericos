e6304db7abb5551546234b40e74b364fc344e958
Daniel Corral <167433136+Korra-5@users.noreply.github.com>
2025-02-07 15:44:54 +0100

    Add files via upload

---
+++ HilosParaTodos/HilosParaTodos/MiHilo.cs
@@ -0,0 +1,30 @@
+namespace HilosParaTodos;
+
+public class MiHilo
+{
+    Thread hilo;
+    private string text;
+    
+    //Pasamos la variable finalizar de la clase Wrapper
+    private Wrapper<Action> finalizar;
+    
+    public MiHilo(string text, Wrapper<Action> finalizar)
+    {
+        this.text = text;
+        this.finalizar = finalizar;
+        finalizar.Value += () => { Console.WriteLine($"Hilo {text}"); };
+        hilo = new Thread(_process);
+    }
+
+    public void Start()
+    {
+        hilo.Start();
+    }
+
+    void _process()
+    {
+        for (int i = 0; i < 1000; i++) Console.Write (text);
+        finalizar.Value.Invoke();
+        Console.WriteLine($"Ha terminado: {text}");
+    }
+}
